fc19b05eb453f020a55fcc070c131cf056c94c0c
Harolicious <Harold.valenzuela@uchile.cl>
2024-07-26 18:14:12 -0400

    Cubitz
    Las scenes de cada modulo para SOFA 23.12.01

---
+++ Scenes/Scene_Shear/generateMeshes.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 14:02:25 2023
+
+@author: stefan
+"""
+
+import gmsh
+import numpy as np
+import Constants
+
+gmsh.initialize()
+
+def defineMeshSizes(lc=0.5):   
+    #-------------------
+    # MeshSizes 
+    #-------------------
+
+    gmsh.model.mesh.field.add("Box", 1)
+    gmsh.model.mesh.field.setNumber(1, "VIn", lc)
+    gmsh.model.mesh.field.setNumber(1, "VOut", lc)
+    gmsh.model.mesh.field.setNumber(1, "XMin", -100)
+    gmsh.model.mesh.field.setNumber(1, "XMax", 100)
+    gmsh.model.mesh.field.setNumber(1, "YMin", -100)
+    gmsh.model.mesh.field.setNumber(1, "YMax", 100)
+    gmsh.model.mesh.field.setNumber(1, "ZMin", -100)
+    gmsh.model.mesh.field.setNumber(1, "ZMax", 100)    
+    gmsh.model.mesh.field.setNumber(1, "Thickness", 0.3)
+     
+    gmsh.model.mesh.field.setAsBackgroundMesh(1)
+    
+    gmsh.option.setNumber("Mesh.CharacteristicLengthExtendFromBoundary", 0)
+    gmsh.option.setNumber("Mesh.CharacteristicLengthFromPoints", 0)
+    gmsh.option.setNumber("Mesh.CharacteristicLengthFromCurvature", 0)
+    
+LadoCubo = Constants.LadoCubo
+AlturaCilindro = Constants.AlturaCilindro
+RadioCilindro = Constants.RadioCilindro
+
+
+BoxTag = gmsh.model.occ.addBox(-LadoCubo/2,0,-LadoCubo/2,LadoCubo, LadoCubo, LadoCubo)
+DimTagBox = (3, BoxTag)
+
+CylinderTag = gmsh.model.occ.addCylinder(0, (LadoCubo-AlturaCilindro)/2,0,0, AlturaCilindro, 0 , RadioCilindro, angle= 2*np.pi)
+DimTagCylinder = (3, CylinderTag)
+DimTagcylinder = gmsh.model.occ.rotate([DimTagCylinder], 0, LadoCubo/2, 0, 0, 0, 1, -np.pi/4)
+
+
+Cutout = gmsh.model.occ.cut([DimTagBox], [DimTagCylinder])
+
+gmsh.model.occ.synchronize()
+
+defineMeshSizes(3)
+gmsh.model.mesh.generate(3)
+# gmsh.model.mesh.refine()
+gmsh.write("CubitoShear.vtk")
+gmsh.fltk.run()
+
+gmsh.clear()
+
+CylinderTag = gmsh.model.occ.addCylinder(0, (LadoCubo-AlturaCilindro)/2,0, 0, AlturaCilindro,0, RadioCilindro, angle= 2*np.pi)
+DimTagCylinder = (3, CylinderTag)
+DimTagcylinder = gmsh.model.occ.rotate([DimTagCylinder], 0, LadoCubo/2, 0, 0, 0, 1, -np.pi/4)
+
+
+gmsh.model.occ.synchronize()
+defineMeshSizes(2)
+gmsh.model.mesh.generate(2)
+gmsh.model.mesh.refine()
+gmsh.write("CubitoShear_Cavity.stl")
+gmsh.fltk.run()
+
+# Visual model
+
+Box1Tag = gmsh.model.occ.addBox(-LadoCubo/2,0,-LadoCubo/2,LadoCubo, LadoCubo, LadoCubo)
+DimTagBox1 = (3, Box1Tag)
+
+
+gmsh.model.occ.synchronize()
+gmsh.model.mesh.generate(2)
+gmsh.model.mesh.refine()
+
+gmsh.write("Cubito_Shear_visu.stl")
