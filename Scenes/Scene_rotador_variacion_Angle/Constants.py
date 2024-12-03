81b4b1a2e7c8f429cd565bc4961998dc5a2b8b47
Harolicious <Harold.valenzuela@uchile.cl>
2024-12-03 18:22:37 -0300

    angle variable

---
+++ Scenes/Scene_rotador_variacion_Angle/Constants.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 16:16:01 2023
+
+@author: tu_sc
+"""
+
+LadoCubo = 20 #mm
+AlturaCilindro = 17
+RadioCilindro = 9
+
+Density = 30
+Density2 = 10
+LevelHeight = AlturaCilindro-2 #13.5
+Repeat = 10
+Deg = 80
+
+Diff = (AlturaCilindro - LevelHeight)/2
+H = (LadoCubo-AlturaCilindro)/2 + Diff
